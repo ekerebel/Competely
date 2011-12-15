ea24f6a261d0d3cb5db2c428d45efb76539d1707
Erwan KEREBEL <erwan@Erwan-KEREBELs-MacBook-Pro.local>
2011-12-15 15:45:59 -0600

    added style, headers, footers

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ Competely::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
